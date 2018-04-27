c3ea2bac11ee673030e3696539d3952ad2ffcbf3
Slobodan <slobodan.kosanovic@gmail.com>
2018-04-27 17:38:41 +0200

    a

--- src/com/kole/Main.java
+++ src/com/kole/Main.java
@@ -6,8 +6,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        MobilePhone.addContact("kole",123456);
-        MobilePhone.printContacts();
 
+        MobilePhone.addContact("sale",123456);
+        MobilePhone.addContact("kole",123456);
+        MobilePhone.addContact("konjina", 98547);
+        MobilePhone.printContacts();
+        System.out.println("*********************************************");
+        boolean test = MobilePhone.checkIfExists("somina");
+        System.out.println(test);
     }
 }
--- src/com/kole/MobilePhone.java
+++ src/com/kole/MobilePhone.java
@@ -2,7 +2,7 @@ package com.kole;
 
 import java.util.ArrayList;
 
-public class MobilePhone extends Contacts{
+public class MobilePhone extends Contacts {
     public static ArrayList<Contacts> contact = new ArrayList<Contacts>();
 
     public MobilePhone(String name, int phoneNumber, ArrayList<Contacts> contacts) {
@@ -10,28 +10,39 @@ public class MobilePhone extends Contacts{
         this.contact = contacts;
     }
 
-    public static void addContact (String name, int number){
-        Contacts contact = new Contacts(name,number);
-        MobilePhone.contact.add(contact);
-        System.out.println("Added contact named " + contact.getName() + " with number " + contact.getPhoneNumber());
+    public static void addContact(String name, int number) {
+        boolean check = checkIfExists(name);
+        if (check==false){
+            Contacts contact = new Contacts(name, number);
+            MobilePhone.contact.add(contact);
+            System.out.println("Added contact named " + contact.getName() + " with number " + contact.getPhoneNumber());
+        } else System.out.println("Cannot add contact as it is already present");
+
+
     }
 
-    public static void printContacts(){
-        for (int i=0; i<contact.size();i++){
-            System.out.println("There is " + contact.size() + " contact(s) in your phone book");
-            System.out.println("At position " + i + " is contact named " + contact.get(i));
-            System.out.println();
+    public static void printContacts() {
+        System.out.println("There is total " + contact.size() + " contact(s) in your phone book");
+        for (int i = 0; i < contact.size(); i++) {
+            System.out.println("At position " + (i + 1) + " is contact named " + contact.get(i).getName() + " with phone number " + contact.get(i).getPhoneNumber());
         }
     }
 
-
-    public boolean queryContact(String name){
-        if (name != null){
-            System.out.println("Contact with name" + name + "already exists!");
-            return true;
-        } else return false;
+    public static boolean checkIfExists(String name){
+        for (int i=0;i<contact.size();i++){
+            if (contact.get(i).getName().contains(name)){
+                return true;
+            }
+        }return false;
     }
 
+    public static void updateContact(){
+
+    }
+
+    public static void removeContact(){
+
+    }
 
 
 }
