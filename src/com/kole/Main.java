b249115bfa35591a5e1ae44230e017c073dd5fc5
Slobodan <slobodan.kosanovic@gmail.com>
2018-04-30 16:43:26 +0200

    a

--- src/com/kole/Main.java
+++ src/com/kole/Main.java
@@ -25,6 +25,33 @@ public class Main {
 //        MobilePhone.printContacts();
         boolean quit = false;
         int choice = 0;
+        printInstructions();
+        while (!quit){
+            System.out.println("Enter your choice: ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 0:
+                    printInstructions();
+                    break;
+                case 1:
+                    MobilePhone.printContacts();
+                    break;
+                case 2:
+                    MobilePhone.addContact(scanner.nextLine(),scanner.nextInt());
+                    break;
+                case 3:
+                    MobilePhone.updateContact(scanner.nextLine(), scanner.nextInt());
+                    break;
+                case 4:
+                    MobilePhone.removeContact(scanner.nextInt());
+                    break;
+                case 5:
+                    quit=true;
+                    break;
+            }
+        }
 
 
     }
